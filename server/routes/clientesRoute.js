06ae310cba40ee319d0cd0b1d4d6f6cd7c3e9d1d
leandro-br-dev <leandro.br.dev@gmail.com>
2020-11-27 16:16:42 -0300

    ajustando rota cliente

--- server/routes/clientesRoute.js
+++ server/routes/clientesRoute.js
@@ -4,10 +4,10 @@ const { Router }        = require('express'),
 
 
     routes.get('/clientes', ClienteController.findAll);
-    routes.get('/clientes/:id', ClienteController.findOne);
+    routes.get('/clientes/:cpf_cnpj', ClienteController.findOne);
     routes.post('/clientes', ClienteController.create);    
     routes.put('/clientes/login/:cpf_cnpj', ClienteController.login);
-    routes.put('/clientes/:id', ClienteController.update);
-    routes.delete('/clientes/:id', ClienteController.delete);
+    routes.put('/clientes/:cpf_cnpj', ClienteController.update);
+    routes.delete('/clientes/:cpf_cnpj', ClienteController.delete);
 
 module.exports = routes;
